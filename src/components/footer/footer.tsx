49d1139ba475c7a3398bfc1f08a058cf65a48f1f
Edson Brandon <edsonbrandon8@gmail.com>
2024-08-27 14:37:10 -0300

    feat: responsive sm

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -25,7 +25,7 @@ export default function Footer() {
       </div>
 
       <hr className="h-0.5" />
-      <div className="flex flex-row justify-between py-10 flex-wrap xsm:justify-center">
+      <div className="flex flex-row justify-between py-10 flex-wrap xsm:justify-center sm:justify-center sm:flex-col sm: items-center">
         <ul className="flex flex-row gap-10 font-thin xsm: text-sm">
           <li>
             <a href="">Home</a>
@@ -37,9 +37,9 @@ export default function Footer() {
             <a href="">Contato</a>
           </li>
         </ul>
-        <p className="flex flex-wrap font-thin xsm:text-sm xsm:justify-center xsm:py-10">
+        <p className="flex flex-col font-thin xsm:text-sm xsm:justify-center xsm:py-10 sm:py-10">
           Projetado e desenvolvido por{" "}
-          <span className="font-semibold bg-gradient-to-r from-cyan-300 to-emerald-500 bg-clip-text text-transparent xsm:text-sm">
+          <span className="font-semibold bg-gradient-to-r from-cyan-300 to-emerald-500 bg-clip-text text-transparent xsm:text-sm sm:text-center">
             Edson Brandon
           </span>
         </p>
