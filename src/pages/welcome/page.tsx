d88b7cffa6d02aaf5ac50356e314ec45bc818afc
Edson Brandon <edsonbrandon8@gmail.com>
2024-08-27 11:13:48 -0300

    refactor: welcome banner error fix

--- src/pages/welcome/page.tsx
+++ src/pages/welcome/page.tsx
@@ -5,23 +5,26 @@ export default function Welcome() {
   return (
     <main className="flex">
       <div className="flex flex-row items-center justify-between w-screen h-screen flex-wrap">
-        <p className="font-bold text-6xl">
-          Olá 👋, <br /> Meu nome é <br />
-          <span className="bg-gradient-to-r from-cyan-300 to-emerald-500 bg-clip-text text-transparent">
-            Edson Brandon
-          </span>
-          <br />
-          e sou um <br />
+        <div>
+          <p className="font-bold text-6xl">
+            Olá 👋, <br /> Meu nome é <br />
+            <span className="bg-gradient-to-r from-cyan-300 to-emerald-500 bg-clip-text text-transparent">
+              Edson Brandon
+            </span>
+            <br />
+            e sou um <br />
+          </p>
           <Typewriter
             options={{
               strings: ["Desenvolvedor mobile.", "Desenvolvedor front-end."],
               autoStart: true,
               loop: true,
               wrapperClassName:
-                "bg-gradient-to-r from-cyan-300 to-emerald-500 bg-clip-text text-transparent",
+                "text-6xl font-bold bg-gradient-to-r from-cyan-300 to-emerald-500 bg-clip-text text-transparent",
             }}
           />
-        </p>
+        </div>
+
         <div className="flex w-50 h-50 rounded-full bg-gradient-to-r from-cyan-300 to-emerald-500 ">
           <img
             className="rounded-full p-2"
